6dc5cdc6ee4c9cec92d5dee5e3cd9c8b76108ea5
mFlaifel <msf-2oo7@hotmail.com>
2020-02-19 15:44:08 +0200

    some frontend validation relates #16

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,27 @@
 
 const search = document.querySelector('.input-recpie');
 const searchbtn = document.querySelector('.search');
+const form = document.querySelector('.form__group');
+const errorDiv = document.createElement('div');
+
+
+// input field validation
+const checkInputValue = (inputValue) => inputValue.replace(/ +(?= )/g, '').trim();
+const disableBtn = () => {
+  if (search.value === '') {
+    searchbtn.disabled = true;
+    const errText = document.createElement('p');
+    errText.textContent = 'Please type your desire food';
+    errorDiv.appendChild(errText);
+  } else {
+    searchbtn.disabled = false;
+  }
+};
 
 searchbtn.addEventListener('click', () => {
-  const body = { search: search.value };
+  disableBtn();
+  const searchValue = checkInputValue(search.value);
+  const body = { search: searchValue };
   getSearchValue(body, '/foo').then((data) => {
     displayData(data);
   }).catch((err) => console.log(err));
